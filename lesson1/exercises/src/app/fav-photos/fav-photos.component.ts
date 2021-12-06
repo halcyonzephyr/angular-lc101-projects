b5dbdbdbe04a7b11fc38c9a52297ebd5f21a565f
halcyonzephyr <gus.stobbs@gmail.com>
2021-12-06 00:00:47 -0600

    lesson 1 exercises complete

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Random Images';
+  image1 = 'https://www.hdnicewallpapers.com/Walls/Big/Dog/Funny_Dog_Puppy_Wrapped_in_Blanket_4K_Wallpaper.jpg';
+  image2 = 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwallpapercave.com%2Fwp%2FxoURTZD.jpg&f=1&nofb=1';
+  image3 = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages.latintimes.com%2Fsites%2Flatintimes.com%2Ffiles%2Fstyles%2Flarge%2Fpublic%2F2014%2F07%2F01%2Ffunny-dogs_2.jpg&f=1&nofb=1';
+  image4 = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.giphy.com%2Fmedia%2FUv0VUrAT6FtMQ%2Fgiphy.gif&f=1&nofb=1'
+
+  alt1 = 'pug in a blanket'
+  alt2 = 'small dog in a raptor suit'
+  alt3 = 'happy dog lifting weights'
 
   constructor() { }
 
