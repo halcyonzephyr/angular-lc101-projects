0bb1ccc25d926bdd58fb7b0624f7c5436c5118e3
Gus <gus.stobbs@gmail.com>
2021-12-06 19:40:38 -0600

    built out Mission Planning Dashboard

---
+++ lesson1/angular_practice/angular-studio-part1/src/app/header/header.component.ts
@@ -0,0 +1,19 @@
+import { AbstractEmitterVisitor } from '@angular/compiler/src/output/abstract_emitter';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  missionName: string = "Mars 2030";
+  rocketName: string = "Plasma Max"
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
